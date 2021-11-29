72516fcdb4dbed5b3a822472146b213fa2fd84d4
kuh6969 <93518896+kuh6969@users.noreply.github.com>
2021-11-29 12:20:32 +0700

    Add files via upload

---
+++ message/example.js
@@ -0,0 +1,77 @@
+exports.useOwner = (prefix) => {
+   return `*「  EXAMPLE OWNER 」*
+cara penggunaan
+`
+}
+
+exports.useDownload = (prefix) => {
+   return `*「  EXAMPLE DOWNLOADER 」*
+*CARA PENGGUNAAN*
+*PERHATIKAN EXAMPLE*
+
+〆 *Command :* ${prefix}fbdl
+〆 *Deskripsi :* Facebook Downloader
+〆 *Example :* ${prefix}fbdl https://facebook.com/xxx
+
+
+〆 *Command :* ${prefix}igdl
+〆 *Deskripsi :* Instagram Downloader
+〆 *Example :* ${prefix}igdl https://www.instagram.com/p/xxxx
+
+
+〆 *Command :* ${prefix}igdl2
+〆 *Deskripsi :* Instagram Downloader Versi 2
+〆 *Example :* ${prefix}igdl https://www.instagram.com/p/xxxx
+
+
+〆 *Command :* ${prefix}tiktok
+〆 *Deskripsi :* Tiktok Downloader
+〆 *Example :* ${prefix}tiktok https://vt.tiktok.com/xxx
+
+
+〆 *Command :* ${prefix}ythd
+〆 *Deskripsi :* YouTube Downloader HD 720p
+〆 *Example :* ${prefix}ythd https://youtu.be/xxx
+
+
+〆 *Command :* ${prefix}ytmp3
+〆 *Deskripsi :* YouTube Downloader MP3
+〆 *Example :* ${prefix}ytmp3 https://youtu.be/xxx
+
+
+〆 *Command :* ${prefix}ytmp4
+〆 *Deskripsi :* YouTube Downloader MP4
+〆 *Example :* ${prefix}ytmp4 https://youtu.be/xxx
+
+
+〆 *Command :* ${prefix}twitter
+〆 *Deskripsi :* Twitter Downloader
+〆 *Example :* ${prefix}twitter https://twitter.
+
+
+〆 *Command :* ${prefix}play
+〆 *Deskripsi :* Mencari Video / Lagu
+〆 *Example :* ${prefix}play Hero - cash cash
+
+
+〆 *Command :* ${prefix} soundcloud
+〆 *Deskripsi :* Soundcloud Downloader
+〆 *Example :* ${prefix} soundcloud https://soundcloud.com/xxx
+
+
+〆 *Command :* ${prefix}tiktoknowm
+〆 *Deskripsi :* TikTok Downloader MP4 No Watermark
+〆 *Example :* ${prefix}tiktoknowm https://vt.tiktok.com/xxx
+
+
+〆 *Command :* ${prefix}tiktokaudio
+〆 *Deskripsi :* TikTok Downloader MP3
+〆 *Example :* ${prefix}tiktokaudio https://vt.tiktok.com/xxx
+
+
+〆 *Command :* ${prefix}mediafire
+〆 *Deskripsi :* MediaFire Downloader
+〆 *Example :* ${prefix}ythd https://mediafire.com/xxx
+`
+}
+
