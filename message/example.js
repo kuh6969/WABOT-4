66dbe79cecc26c281300ee21e3810ba33256d63d
kuh6969 <93518896+kuh6969@users.noreply.github.com>
2021-11-29 16:23:11 +0700

    Add files via upload

--- message/example.js
+++ message/example.js
@@ -1,13 +1,13 @@
+// Menu Owner
 exports.useOwner = (prefix) => {
    return `*「  EXAMPLE OWNER 」*
-cara penggunaan
+coming soon yo, ngetik capek... support owner dengan cara topup🗿
 `
 }
 
+// Menu Downloader
 exports.useDownload = (prefix) => {
    return `*「  EXAMPLE DOWNLOADER 」*
-*CARA PENGGUNAAN*
-*PERHATIKAN EXAMPLE*
 
 〆 *Command :* ${prefix}fbdl
 〆 *Deskripsi :* Facebook Downloader
@@ -75,3 +75,278 @@ exports.useDownload = (prefix) => {
 `
 }
 
+// Menu Info
+exports.useInfo = (prefix) => {
+   return `*「  EXAMPLE INFO 」*
+
+〆 *Command :* ${prefix}bugreport
+〆 *Deskripsi :* Lapor Bug Ke Owner
+〆 *Example :* ${prefix}bugreport fitur sticker error
+`
+}
+
+// Menu Sticker
+exports.useSticker = (prefix) => {
+   return `*「  EXAMPLE STICKER 」*
+
+〆 *Command :* ${prefix}attp
+〆 *Deskripsi :* Teks to Sticker
+〆 *Example :* ${prefix}attp wibu wibu
+
+
+〆 *Command :* ${prefix}ttp2
+〆 *Deskripsi :* Teks to Sticker
+〆 *Example :* ${prefix}ttp2 wibu wibu
+
+
+〆 *Command :* ${prefix}ttp3
+〆 *Deskripsi :* Teks to Sticker
+〆 *Example :* ${prefix}ttp3 wibu wibu
+
+
+〆 *Command :* ${prefix}ttp4
+〆 *Deskripsi :* Teks to Sticker
+〆 *Example :* ${prefix}ttp wibu wibu
+
+
+〆 *Command :* ${prefix}semoji
+〆 *Deskripsi :* Emote to Sticker
+〆 *Example :* ${prefix}semoji 🗿
+
+
+〆 *Command :* ${prefix}sticker
+〆 *Deskripsi :* Image to Sticker
+〆 *Example :* Kirim/tag gambar dengan caption ${prefix}sticker
+
+
+〆 *Command :* ${prefix}smeme
+〆 *Deskripsi :* Sticker Meme
+〆 *Example :* ${prefix}smeme miku|beban
+
+
+〆 *Command :* ${prefix}swm
+〆 *Deskripsi :* Sticker Watermark
+〆 *Example :* ${prefix}swm elaina|wangy
+`
+}
+
+// Menu Islam
+exports.useIslam = (prefix) => {
+   return `*「  EXAMPLE ISLAM 」*
+   
+〆 *Command :* ${prefix}kisahnabi *<nabi>*
+〆 *Info :* untuk nama Nabi pakai huruf kecil ya
+〆 *Example :* ${prefix}kisahnabi muhammad
+
+
+〆 *Command :* ${prefix}jadwalsholat *<daerah>*
+〆 *Deskripsi :* Mengetahui Jam Sholat
+〆 *Example :* ${prefix}jadwalsholat Semarang
+
+
+〆 *Command :* ${prefix}jadwalsholat2 *<daerah>*
+〆 *Deskripsi :* Mengetahui Jam Sholat Versi 2
+〆 *Example :* ${prefix}jadwalsholat Semarang
+
+
+〆 *Command :* ${prefix}alquranaudio *<surah>*
+〆 *Deskripsi :* Mengirimkan Surah File Audio
+〆 *Example :* ${prefix}alquranaudio al-mulk
+`
+}
+
+// Menu Sertifikat
+exports.useSerti = (prefix) => {
+   return `*「  EXAMPLE SERTI 」*
+   
+ga ada yang perlu dikasih contoh
+
+(support owner dengan cara topup 😎🗿)
+`
+}
+
+// Menu Cerita
+exports.useCerita = (prefix) => {
+   return `*「  EXAMPLE CERITA 」*
+
+ga ada yang perlu dikasih contoh
+
+(support owner dengan cara topup 😎🗿)
+
+`
+}
+
+// Menu Maker
+exports.useMaker = (prefix) => {
+   return `*「  EXAMPLE MAKER 」*
+   
+   fitur ini masih error, kapan2 aja gw fix
+
+(support owner dengan cara topup 😎🗿)
+`
+}
+
+// Menu Tools
+exports.useTools = (prefix) => {
+   return `*「  EXAMPLE TOOLS 」*
+   
+   ga ada yang perlu dikasih contoh
+
+(support owner dengan cara topup 😎🗿)
+`
+}
+
+// Menu Other
+exports.useOther = (prefix) => {
+   return `*「  EXAMPLE OTHER 」*
+   
+〆 *Command :* ${prefix}brainly *<query>*
+〆 *Deskripsi :* Maaf Kalo Salah🗿
+〆 *Example :* ${prefix}brainly pengertian wibu😱
+
+
+〆 *Command :* ${prefix}artinama *<nama>*
+〆 *Deskripsi :* Arti Dari Nama Kamu
+〆 *Example :* ${prefix}artinama Elaina
+
+
+〆 *Command :* ${prefix}happymod *<query>*
+〆 *Deskripsi :* Mencari Aplikasi Mod
+〆 *Example :* ${prefix}happymod epep mex
+
+
+〆 *Command :* ${prefix}moddroid *<query>*
+〆 *Deskripsi :* Mencari Aplikasi Mod
+〆 *Example :* ${prefix}moddroid gopay mo
+
+
+〆 *Command :* ${prefix}playstore *<query>*
+〆 *Deskripsi :* Mencari Aplikasi Di Playstore
+〆 *Example :* ${prefix}playstore Mobile Legends
+
+
+〆 *Command :* ${prefix}ssweb  *<query>*
+〆 *Deskripsi :* Screenshot Website
+〆 *Example :* ${prefix}ssweb https://nekopoi.care/
+
+
+〆 *Command :* ${prefix}google *<query>*
+〆 *Deskripsi :* Google Search
+〆 *Example :* ${prefix}google milf
+
+
+〆 *Command :* ${prefix}image *<query>*
+〆 *Deskripsi :* Image Search
+〆 *Example :* ${prefix}image loli
+
+
+〆 *Command :* ${prefix}pinterest *<query>*
+〆 *Deskripsi :* Pinterest Search
+〆 *Example :* ${prefix}pinterest loli wangy
+
+
+〆 *Command :* ${prefix}img2url *<reply img>*
+〆 *Deskripsi :*  Mengubah Gambar ke Url
+〆 *Example :* tag/kirim gambar dengan caption ${prefix}img2url
+
+
+〆 *Command :* ${prefix}ytsearch *<query>*
+〆 *Deskripsi :* YouTube Search
+〆 *Example :* ${prefix}ytsearch Rick Roll
+`
+}
+
+// Menu Group
+exports.useGroup = (prefix) => {
+   return `*「  EXAMPLE GROUP 」*
+   
+〆 *Command :* ${prefix}gelud *<@tag>*
+〆 *Deskripsi :* Gelud Dengan Member Grub
+〆 *Example :* ${prefix}gelud @62xxx
+
+
+〆 *Command :* ${prefix}tictactoe *<@tag>*
+〆 *Deskripsi :* Game Tictactoe
+〆 *Example :* ${prefix}tictactoe @62xxx
+
+
+〆 *Command :* ${prefix}promote *<@tag>*
+〆 *Deskripsi :* Promote Member Grub
+〆 *Example :* ${prefix}promote @62xxx
+
+
+〆 *Command :* ${prefix}demote *<@tag>*
+〆 *Deskripsi :* Demote Member Grub
+〆 *Example :* ${prefix}demote @62xxx
+
+
+〆 *Command :* ${prefix}kick *<@tag>*
+〆 *Deskripsi :* Kick Member Grub
+〆 *Example :* ${prefix}kick @62xxx
+
+
+〆 *Command :* ${prefix}add *<nomer>*
+〆 *Deskripsi :* Add Member To Grub
+〆 *Example :* ${prefix}add @628123xxx
+
+
+〆 *Command :* ${prefix}sider *<reply chat bot>*
+〆 *Deskripsi :* Sider
+〆 *Example :* kirim ${prefix}sider dan reply chat bot
+
+
+〆 *Command :* ${prefix}hidetag *<teks>*
+〆 *Deskripsi :* Tag All Member Grub
+〆 *Example :* ${prefix}hidetag hallo
+`
+}
+
+
+
+// Menu Fun
+exports.useFun = (prefix) => {
+   return `*「  EXAMPLE FUN 」*
+   
+   coming soon yo, ngetik capek... support owner dengan cara topup🗿
+   `
+}
+
+// Menu Wibu
+exports.useWibu = (prefix) => {
+   return `*「  EXAMPLE WIBU 」*
+
+coming soon yo, ngetik capek... support owner dengan cara topup🗿
+`
+}
+
+// Menu Kristen
+exports.useKristen = (prefix) => {
+   return `*「  EXAMPLE KRISTEN 」*
+
+coming soon yo, ngetik capek... support owner dengan cara topup🗿
+`
+}
+
+// Menu Stalking
+exports.useStalk = (prefix) => {
+   return `*「  EXAMPLE STALK 」*
+
+coming soon yo, ngetik capek... support owner dengan cara topup🗿
+`
+}
+
+// Menu URL
+exports.useUrl = (prefix) => {
+   return `*「  EXAMPLE URL 」*
+
+coming soon yo, ngetik capek... support owner dengan cara topup🗿
+`
+}
+
+// Menu Information
+exports.useInformation = (prefix) => {
+   return `*「  EXAMPLE INFORMATION 」*
+
+coming soon yo, ngetik capek... support owner dengan cara topup🗿
+`
+}
